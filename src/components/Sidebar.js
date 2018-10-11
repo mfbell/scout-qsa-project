914bddbc25b3727225d034c8e29c9720acbc9f47
mtech <kind.of.random@outlook.com>
2018-10-11 19:02:00 +0100

    Removed state and included opening button to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,59 +1,28 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
-    constructor(props) {
-        super(props);
+  handleClose() {
+    document.getElementById("sidebar").style.width = "0";
+  }
 
-        this.handleClose = this.handleClose.bind(this);
-    }
+  handleOpen() {
+    document.getElementById("sidebar").style.width = "300px";
+  }
 
-    handleClose(e) {
-        this.props.close(e);
-    }
-
-    render() {
-        return (
-            <nav class="sidebar" style={{width: this.props.isOpen ? "250px" : "0"}}>
-                <a href onClick={this.handleClose}>&times;</a>
-                <a href="/about">About</a>
-                <a href="/servers">Services</a>
-                <a href="/clients">Clients</a>
-                <a href="/contact">Contact</a>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <>
+        <span className="text-white" onClick={this.handleOpen}>&#9776;</span>
+        <nav class="sidebar" id="sidebar">
+          <a href onClick={this.handleClose}>&times;</a>
+          <a href="/about">About</a>
+          <a href="/servers">Services</a>
+          <a href="/clients">Clients</a>
+          <a href="/contact">Contact</a>
+        </nav>
+      </>
+    );
+  }
 }
 
-class TextButton extends Component {
-    /* Text Button
-        Props: 
-        open - callback on click
-
-        children - text to display
-    */
-    constructor(props) {
-        super(props);
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(e) {
-        this.props.open(e)
-    }
-
-    render() {
-        return <span onClick={this.handleClick}>{this.props.children}</span>
-    }
-}
-
-class SidebarOpener extends Component {
-    /* Burger Button
-        Props: open - function to call on click
-    */
-
-    render() {
-        return <TextButton open={this.props.open}>&#9776;</TextButton>
-    }
-}
-
-export {Sidebar, SidebarOpener, TextButton}
+export default Sidebar;
