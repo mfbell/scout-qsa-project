fa67acb1bf7794a38b1788d4dc0a342ff489cfd7
mtech <kind.of.random@outlook.com>
2018-10-11 19:02:55 +0100

    Removed sidebar state

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-import {Sidebar, SidebarOpener} from './Sidebar.js'
+import NavBar from './NavBar.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSidebarOpen: false,
-    };
-
-    this.handleSidebarOpen = this.handleSidebarOpen.bind(this)
-    this.handleSideBarClose = this.handleSideBarClose.bind(this)
-  }
-
-  handleSidebarOpen(e) {
-    this.setState({isSidebarOpen: true})
-  }
-
-  handleSideBarClose(e) {
-    this.setState({isSidebarOpen: false})
-  }
-
   render() {
     return (
       <div> 
+        <NavBar />
         my app text
-        <SidebarOpener open={this.handleSidebarOpen}/>
-        <Sidebar isOpen={this.state.isSidebarOpen} close={this.handleSideBarClose} />
       </div>
     );
   }
