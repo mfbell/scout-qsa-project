b869403d9ae4b7bd395d9634ed2a6e63f87aaca5
mtech <kind.of.random@outlook.com>
2018-10-17 16:58:06 +0100

    Added idea for 404s

--- src/components/Errors.js
+++ src/components/Errors.js
@@ -1,5 +1,16 @@
+import React from 'react'
+
 function NoMatch() {
-  return "404: NoMatch Error";
+  return (
+    <>
+      <h1>Where's Timmy gone? We've lost Timmy!</h1>
+      <p>404 Error: Can't find Timmy, time to fill out some paperwork...</p>
+      <form>
+        <label for="search">Search for Timmy</label>
+        <input type="search" id="search"></input>
+      </form>
+    </>
+  )
 }
 
 export { NoMatch };
