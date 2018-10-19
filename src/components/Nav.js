bd9e68df6fdf8370180a75d3ca0ed85fa16337b7
mtech <kind.of.random@outlook.com>
2018-10-19 18:03:40 +0100

    Made changes to style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,8 @@ import {
   NavItem,
   NavLink,
   Form,
-  Input
+  Input,
+  Container
 } from 'reactstrap';
 
 
@@ -32,22 +33,27 @@ class Navbar extends Component {
 
   render() {
     return (
-      <RSNavBar expand="sm">
-        <NavbarBrand to="/" tag={RRNavLink}>SABD</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Form>
-            <Input type="search" placeholder="Explorer - Discover - Share" />
-          </Form>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink tag={RRNavLink} to="/saved" activeClassName="active">Saved</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink tag={RRNavLink} to="/sign-in" activeClassName="active">Sign in</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
+      <RSNavBar expand="sm" {...this.props} light>
+        <Container>
+          <NavbarBrand to="/" tag={RRNavLink} exact>SABD</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Form>
+              <Input type="search" placeholder="Explorer - Discover - Share" />
+            </Form>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="mx-3">
+                <NavLink tag={RRNavLink} to="/saved" activeClassName="active">Saved</NavLink>
+              </NavItem>
+              <NavItem className="mx-3">
+                <NavLink tag={RRNavLink} to="/sign-in" activeClassName="active">Sign in</NavLink>
+              </NavItem>
+              <NavItem className="mx-3">
+                <NavLink tag={RRNavLink} to="/sign-up" className="btn btn-outline-primary" activeClassName="active">Sign up</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
       </RSNavBar>
     );
   }
@@ -81,28 +87,28 @@ class SideNav extends Component {
 class Footer extends Component {
   render() {
     return (
-      <footer className="fluid-container bg-secondary text-light py-4 px-3 px-sm-5">
+      <footer className="fluid-container bg-light text-dark py-4 px-3 px-sm-5">
         <div className="row">
           <div className="col-12 col-sm-8">
             <p className="lead">We believe in making activity planning easy, with our huge catalog of activities you can find the right one for any event, even when the weather turns bad.</p>
           </div>
           <div className="col-12 col-sm-4 order-sm-first">
             <h3>SADB</h3>
-            Built by <NavLink to="/about-us" className="text-white align-bottom">Max Bell</NavLink>
+            Built by <NavLink to="/about-us" className="text-dark align-bottom">Max Bell</NavLink>
           </div>
         </div>
         <div className="row text-center m-4">
           <div className="col-12 col-sm-4 col-md-3 col-lg-2 ml-auto my-2">
-            <NavLink to="/about-us" className="text-white">About us</NavLink>
+            <NavLink to="/about-us" className="text-dark">About us</NavLink>
           </div>
           <div className="col-12 col-sm-4 col-md-3 col-lg-2 my-2">
-           <NavLink to="/me" className="text-white">My account</NavLink>
+           <NavLink to="/me" className="text-dark">My account</NavLink>
           </div>
           <div className="col-12 col-sm-4 col-md-3 col-lg-2 mr-auto my-2">
-           <NavLink to="/legal" className="text-white">Legal</NavLink>
+           <NavLink to="/legal" className="text-dark">Legal</NavLink>
           </div>
         </div>
-        <div className="row text-center text-white-50">
+        <div className="row text-center text-dark-50">
           <div className="col">&copy; Scout Activity Database</div>
         </div>
       </footer>
