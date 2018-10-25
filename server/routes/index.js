d3425cedceb80c1c5daaae397046be7634ddc47f
mtech <kind.of.random@outlook.com>
2018-10-25 17:57:35 +0100

    put api routes in own module

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const path = require('path');
 
-const acitivtiesRouter = require('./activities');
-const riskAssessmentRouter = require('./risk-assessments');
-const usersRouter = require('./users');
-const authRouter = require('./auth');
+const apiRouter = require('./api');;
 
 router = express.Router()
 
 // Serve the static files from the React app
-router.use(express.static(path.join(__dirname, 'client/build')));
+//router.use(express.static(path.join(__dirname, 'client/build')));
 
 // Routes
-router.route('/api')
-  .use('/activities', acitivtiesRouter)
-  .use('/risk-assessments', riskAssessmentRouter)
-  .use('/users', usersRouter);
+router.use('/api', apiRouter)
 
-// Let React app handle other paths
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../../client/build/index.html'));
-});
+/* Let React app handle other paths
+ * router.get('*', (req, res) => {
+ * res.sendFile(path.join(__dirname + '../../client/build/index.html'));
+ * });
+ */
 
 module.exports = router
