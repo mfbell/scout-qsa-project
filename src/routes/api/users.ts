3a7cdcbe9df8e17da9cc396e35279d845d2f1594
mtech <kind.of.random@outlook.com>
2018-11-04 19:55:54 +0000

    publicID to publicId

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import user from '../../controllers/user';
+import * as user from '../../controllers/user';
 
 const router: Router = Router();
 
 router.route('')
   .post(user.create)
   .get(user.search);
-router.route('/:publicID')
+router.route('/:publicId')
   .get(user.get)
   .put(user.update)
   .delete(user.remove);
