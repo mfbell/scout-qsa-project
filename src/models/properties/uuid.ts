6e7f6e8c27ee4b7bfb9b51c0bced33ab217e8349
mtech <kind.of.random@outlook.com>
2018-11-08 20:51:24 +0000

    Implimented custom bson uuid property

---
+++ src/models/properties/uuid.ts
@@ -0,0 +1,39 @@
+import { Binary } from 'mongodb';
+import uuidv4 from 'uuid/v4';
+import { isUUID } from 'validator';
+
+export function validate(uuid: string, strong: boolean = true) {
+  if (!isUUID(uuid, 4)) {
+    if (strong) throw Error(`Invalid UUID.`);
+    return false;
+  } 
+  return true;
+}
+
+export function parse(uuid: string) {
+  uuid = uuid.toLowerCase()
+  validate(uuid)
+  uuid = uuid.replace(/-/g, '')
+  return new Binary(Buffer.from(uuid, 'hex'), Binary.SUBTYPE_UUID)
+}
+
+export function unparse(binaryUuid: Binary) {
+  const positions = [[0, 4], [4, 6], [6, 8], [8, 10], [10, 16]]
+  return positions.map(pos => {
+    binaryUuid.read(pos[0], pos[1]).toString('hex')
+  }).join('-');
+}
+
+export function bsonUuid() {
+  const uuid = uuidv4(null, new Buffer(16));
+  return new Binary(uuid, Binary.SUBTYPE_UUID);
+}
+
+export default {
+  type: Buffer,
+  unique: true,
+  required: true,
+  default: bsonUuid,
+  set: parse,
+  get: unparse
+}
