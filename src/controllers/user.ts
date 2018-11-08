15e4620ae534979379f4c6780c949803a9e2dd32
mtech <kind.of.random@outlook.com>
2018-11-08 20:51:41 +0000

    added user interface

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from 'express';
 import User from '../models/user';
+import { controller } from './controller';
 
-/**
-  * Get user by res.params.publicId 
-  * @param {Request} req express request object
-  * @param {Response} res express response object 
-  */
-function get(req: Request, res: Response) {
+const get: controller = function get(req, res) {
   User.findOne({ publicId: req.params.publicId })
     .then((user) => res.status(200).json(user))
     .catch((err: Error) => {
@@ -21,8 +17,8 @@ function create(req: Request, res: Response) {
   });
   user.setPassword(req.body.password);
   user.save()
-    .then((user: User) => {
-      res.location(`/api/users/${user.publicID}`);
+    .then((user) => {
+      res.location(`/api/users/${user.publicId}`);
       res.status(200).json({token: user.toAuthJSON.token});
     })
     .catch((err: Error) => {
@@ -36,7 +32,7 @@ function update(req: Request, res: Response) {
     { publicID: req.params.publicID }, 
     req.body, 
     { runValidators: true }
-  ).then((user: User) => res.status(204).end())
+  ).then((user) => res.status(204).end())
   .catch((err: Error) => {
     res.status(400).end();
     console.error(err)
