aa22e416e97b947627dfbd0307d2c7011fb1e95b
mtech <kind.of.random@outlook.com>
2018-11-16 20:06:21 +0000

    OAuth schemas moved

---
+++ src/models/user/auth/oauth.ts
@@ -0,0 +1,24 @@
+import { prop, Typegoose } from 'typegoose';
+
+const required = true;
+
+export class OAuth extends Typegoose {
+  @prop({ required })
+  id!: String;
+  @prop({ required, default: Date })
+  dateAdded!: Date;
+}
+
+export class Facebook extends OAuth {
+  @prop({ required })
+  accessToken!: string;
+  @prop({ required })
+  refreshToken!: string;
+}
+
+export class Twitter extends OAuth {
+  @prop({ required })
+  token!: string;
+  @prop({ required })
+  tokenSecret!: string;
+}
