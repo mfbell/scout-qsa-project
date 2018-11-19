179eda460d26a6af6e2ad9ba6c1c64cb8821f7af
mtech <kind.of.random@outlook.com>
2018-11-19 17:07:42 +0000

    removed undeveloped thing

--- src/routes/api/risk-assessments.ts
+++ src/routes/api/risk-assessments.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import * as riskAssessments from '../../controllers/risk-assessments';
-import { required } from '../../auth';
 
 const router: Router = Router();
 
 router.route('')
-  .post(required, riskAssessments.create);
+  .post(riskAssessments.create);
 router.route('/:id')
   .get(riskAssessments.get)
-  .put(required, riskAssessments.update)
-  .delete(required, riskAssessments.remove);
+  .put(riskAssessments.update)
+  .delete(riskAssessments.remove);
 
 export default router;
