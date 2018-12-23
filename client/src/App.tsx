b228c725a8ce67c185c06782b0aaf6ffbbfd3819
mtech <kind.of.random@outlook.com>
2018-12-23 21:47:23 +0000

    Added the container view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,51 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch
   } from 'react-router-dom';
+import { Container } from 'reactstrap';
 import * as Activities from './activities';
 import { NoMatch } from './errors';
 import { GraphQLClient } from './graphql';
+import { Footer, Navbar } from './nav';
 import { Home } from './us/Home';
 import AboutUs from './us/info/AboutUs';
 import * as Users from './users';
 
-export default class App extends Component {
-  public render() {
-    return (
-      <GraphQLClient>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about-us" component={AboutUs} />
+const App: FunctionComponent = () => {
+  return (
+    <GraphQLClient>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
 
-            <Route path="/sign-in" component={Users.SignIn} />
-            <Route path="/sign-up" component={Users.SignUp} />
-            <Route path="/my-account" component={Users.MyAccount} />
+          <Route component={() =>
+            <>
+              <Navbar />
+              <Container>
+                <Switch>
+                  <Route path="/about-us" component={AboutUs} />
 
-            <Route path="/a/:slug" component={Activities.Activity} />
-            <Route path="/create" component={Activities.Create} />
-            <Redirect exact from="/a" to="/create" />
+                  <Route path="/sign-in" component={Users.SignIn} />
+                  <Route path="/sign-up" component={Users.SignUp} />
+                  <Route path="/my-account" component={Users.MyAccount} />
 
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
-      </GraphQLClient>
-    );
-  }
-}
+                  <Route path="/a/:slug" component={Activities.View} />
+                  <Route path="/create" component={Activities.Create} />
+                  <Redirect exact from="/a" to="/create" />
+
+                  <Route component={NoMatch} />
+                </Switch>
+              </Container>
+              <Footer />
+            </>
+          } />
+        </Switch>
+      </Router>
+    </GraphQLClient>
+  );
+};
+
+export default App;
