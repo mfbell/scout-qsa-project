5d75b86452e15c83ef4ad14d8b385646493dd429
mtech <kind.of.random@outlook.com>
2018-12-23 21:09:12 +0000

    moved and removed stuff

--- client/jest-learn/src/client/index.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from 'react-dom';
-import * as React from 'react';
-import Hello from './components/Hello';
-
-render(<Hello />, document.getElementById('app'))
--- client/ts-starter/src/components/Hello.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Hello from './Hello';
-
-it('renders to correct text when no enthusiasm level is given', () => {
-    const hello = shallow(<Hello name="D" />);
-    expect(hello.find(".greeting").text()).toEqual("Hello D!");
-});
